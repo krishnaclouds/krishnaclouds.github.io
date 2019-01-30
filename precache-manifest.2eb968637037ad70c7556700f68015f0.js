4d369dddbe25aa23ee110e1f011a750a173ee42e
Bala Krishna <balakrishna@docsapp.in>
2019-01-30 18:03:35 +0530

    Updates

---
+++ precache-manifest.2eb968637037ad70c7556700f68015f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/store/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "4d028fbd7a5f367c14db",
+    "url": "/store/static/js/runtime~main.4d028fbd.js"
+  },
+  {
+    "revision": "46d9c001a6358bfd2de6",
+    "url": "/store/static/js/main.46d9c001.chunk.js"
+  },
+  {
+    "revision": "9baff8d41b753e286670",
+    "url": "/store/static/js/1.9baff8d4.chunk.js"
+  },
+  {
+    "revision": "46d9c001a6358bfd2de6",
+    "url": "/store/static/css/main.29a00bf6.chunk.css"
+  },
+  {
+    "revision": "9baff8d41b753e286670",
+    "url": "/store/static/css/1.8e75b2b2.chunk.css"
+  },
+  {
+    "revision": "6cc3e654685b8be86c8b7979def3ecb7",
+    "url": "/store/index.html"
+  }
+];
